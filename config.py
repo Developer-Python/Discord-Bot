eed85ee6e08f7541e84e06971770ecc4aa729c69
Евгений Орлов <60120830+Developer-Python@users.noreply.github.com>
2021-04-21 15:45:53 +0300

    v0.0.1

---
+++ config.py
@@ -0,0 +1,26 @@
+
+settings = {
+    'token': 'ODM0MTI3MjY2MTkwNzIxMDg0.YH8XjQ.yuNO_HAoMKqwcjPXspy38o-sJCk',
+    'bot': 'SAMURAI',
+    'post_id': 834127266190721084,
+    'prefix': '!'
+}
+
+roles = {
+    'awp':834271346816581634,
+    'rifler':834271450272890932,
+    'commander':834271507990577153,
+    'top_friger':834271963198652427,
+    'style_aggresive':834272246633332747,
+    'style_passive':834272711307427870,
+    'style_aggresive_passive':834273895041204284,
+    'global':834272899724607538,
+    'suprime':834273600226983936,
+    'lem':834274143251202111,
+    'legendary_eagle':834274357060698122,
+    'big_star':834274220930367549,
+}
+
+excroles = ()
+
+max_roles_per_user = 2
