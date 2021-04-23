eaa88c007374176dde2eabd4151954e0c67b7c3d
Евгений Орлов <60120830+Developer-Python@users.noreply.github.com>
2021-04-23 19:35:18 +0300

    v0.0.3

--- config.py
+++ config.py
@@ -1,34 +1,6 @@
-
 settings = {
     'token': 'ODM0MTI3MjY2MTkwNzIxMDg0.YH8XjQ.yuNO_HAoMKqwcjPXspy38o-sJCk',
     'bot': 'SAMURAI',
-    'post_id': 834127266190721084,
+    'id': 834127266190721084,
     'prefix': '!'
 }
-
-roles = {
-    'admin':834280424389214229,
-    'vip':834280524855902279,
-    'confirmed_player':834281064041152565,
-    'awp':834271346816581634,
-    'rifler':834271450272890932,
-    'commander':834271507990577153,
-    'top_friger':834271963198652427,
-    'style_aggresive':834272246633332747,
-    'style_passive':834272711307427870,
-    'style_aggresive_passive':834273895041204284,
-    'kill_25':834274143251202111,
-    'kill_30':834274357060698122,
-    'kill_35':834274220930367549,
-    'mirage':834446713505710122,
-    'dust_2':834447389968433173,
-    'inferno':834448348861693965,
-    'overpass':834448402162647050,
-    'nuke':834448446668013608,
-    'train':834448490645028974,
-    'cache':834448718089420821,
-}
-
-excroles = ()
-
-max_roles_per_user = 12
