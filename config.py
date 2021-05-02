0eff3c5f35ea9d6f9dd2949b973d3c5b8bbc519b
Evgeniy Orlov <60120830+Developer-Python@users.noreply.github.com>
2021-05-02 15:34:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'ODM0MTI3MjY2MTkwNzIxMDg0.YH8XjQ.yuNO_HAoMKqwcjPXspy38o-sJCk',
-    'bot': 'SAMURAI',
-    'id': 834127266190721084,
+    'token': ' >>> ваш токен <<< ',
+    'bot': '>>> ваше имя бота <<<',
+    'id': ,
     'prefix': '!'
 }
